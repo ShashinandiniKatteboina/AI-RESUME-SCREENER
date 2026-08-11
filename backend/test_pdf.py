e8320885f7ed90b659a8cd6ffab9255b302e2994
Shashi Nandini <shashinandinikatteboina@gmail.com>
2026-08-11 07:22:01 +0530

    test files deleted

--- backend/test_pdf.py
+++
@@ -1,9 +0,0 @@
-from services.pdf_service import extract_text_from_pdf
-
-
-pdf_path = "uploads/23b81a05dp_resume-2.pdf"
-
-text = extract_text_from_pdf(pdf_path)
-
-print("===== EXTRACTED TEXT =====")
-print(text)
